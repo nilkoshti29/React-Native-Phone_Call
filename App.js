c7987f846e667905873b76001cffe6e093773767
nilkoshti29 <53292687+nilkoshti29@users.noreply.github.com>
2020-01-27 13:26:21 +0530

    Phone_Call
    Phone_Call

---
+++ App.js
@@ -0,0 +1,45 @@
+import React,{Component } from 'react';
+import { StyleSheet,Text,View,TouchableOpacity,Linking,Platform } from 'react-native';
+
+export default class App extends Component{
+  dialCall = () =>{
+    let phoneNumber = '';
+
+    if(Platform.OS === 'android'){
+      phoneNumber='tel:${1234567890}';
+    }
+    else{
+      phoneNumber = 'telprompt:${1234567890}';
+    }
+    Linking.openURL(phoneNumber);
+  };
+
+    render(){
+      return(
+        <View style={styles.MainContainer}>
+          <TouchableOpacity onPress={this.dialCall}activeOpacity={0.7} style={styles.button}>
+            <Text style={styles.TextStyle}>OPEN PHONE NUMBER IN DIAL SCREEN</Text>
+          </TouchableOpacity>
+        </View>
+      );
+    }
+}
+
+const styles = StyleSheet.create({
+  MainContainer:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  button:{
+    width:'80%',
+    padding:6,
+    backgroundColor:'#FF6F00',
+    borderRadius:7,
+  },
+  TextStyle:{
+    color:'#fff',
+    fontSize:18,
+    textAlign:'center',
+  },
+});
